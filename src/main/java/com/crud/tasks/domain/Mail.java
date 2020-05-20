58f60a6cb4b58360b205770e853f315444e3d2ae
Mateusz Ryndak <kontakt@mryndak.pl>
2020-05-20 18:44:50 +0200

    19.3 - zadanie

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,12 +1,13 @@
 package com.crud.tasks.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class Mail {
     private final String mailTo;
     private final String subject;
     private final String message;
+    private final String toCC;
 }
