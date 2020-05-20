32a7f9676e7755df8b2ca5d1a2be1bc885ef7fd2
Mateusz Ryndak <kontakt@mryndak.pl>
2020-05-20 11:20:30 +0200

    14.3 - lombok, DTOs, main

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,5 +1,6 @@
 package com.crud.tasks;
 
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
+		TaskDto taskDto = new TaskDto(
+				1L,
+				"Test title",
+				"I want to be a coder!"
+		);
+		Long id = taskDto.getId();
+		String title = taskDto.getTitle();
+		String content = taskDto.getContent();
+		System.out.println(id + " " + title + " " + content);
+//		SpringApplication.run(TasksApplication.class, args);
 	}
 
 }
