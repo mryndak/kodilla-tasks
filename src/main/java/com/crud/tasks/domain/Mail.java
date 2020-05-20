1dd72c70a8e3ba66625eaea20995a4439bb57894
Mateusz Ryndak <kontakt@mryndak.pl>
2020-05-20 20:13:13 +0200

    19.4

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,10 +1,13 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class Mail {
     private final String mailTo;
     private final String subject;
