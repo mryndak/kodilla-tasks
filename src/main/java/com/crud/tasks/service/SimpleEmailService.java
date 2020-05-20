978da3ff85d5d7c15d55ad670a59c827bf22cf8e
Mateusz Ryndak <kontakt@mryndak.pl>
2020-05-20 18:37:56 +0200

    19.3 - email; part 2

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
@@ -11,22 +12,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
-
     private final JavaMailSender javaMailSender;
 
-    public void send(final String receiverEmail, final String subject, final String message) {
+    public void send(final Mail mail) {
         log.info("Starting email preparation...");
-
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(receiverEmail);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(message);
-
+            SimpleMailMessage mailMessage = createMailMessage(mail);
             javaMailSender.send(mailMessage);
             log.info("Email has been sent.");
         } catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
         }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        return mailMessage;
     }
 }
